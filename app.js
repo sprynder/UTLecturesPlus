60bd11ef9d124e439e9019acd5ebabfc3261a8d4
James Liu <liujames2003@gmail.com>
2024-03-24 21:13:53 -0500

    autoscroll button

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function sleep(ms) {
 function timestampToMilliseconds(timestamp) {
   // Split the timestamp into hours, minutes, seconds, and seconds
   const [hours, minutes, secondsWithMillis] = timestamp.split(':');
-  
+
   // Split secondsWithMillis into seconds and seconds
   const [seconds, milliseconds] = secondsWithMillis.split('.');
 
@@ -16,118 +16,136 @@ function timestampToMilliseconds(timestamp) {
     (parseInt(seconds, 10) * 1000) +            // seconds to milliseconds
     parseInt(milliseconds, 10);                // milliseconds
 
-  return totalMilliseconds/1000;
+  return totalMilliseconds / 1000;
 }
 
-chrome.runtime.onMessage.addListener(async function (msg, sender, sendResponse) {
+let scroll = true;
 
-  //console.log(msg)
-  if (msg.captions) {
-    //modified_captions = addNewlineBeforeTimestamps(msg.captions)
-    //console.log(msg.captions)
-    //console.log(msg.captions)
-    if(msg.source.includes("utexas"))
-    {
-      let caption_divs = [];
-      let captions = msg.captions;
-      let caption_chunks = captions.split("\n\n");
-      caption_chunks.shift();
-      for (chunk of caption_chunks)
-      {
-        let s = chunk.split("\n");
-        let timestamp = s[0].split(" ")[0];
-        let cap = s[1];
-        let milliseconds = timestampToMilliseconds(timestamp);
-        caption_divs.push({timestamp: timestamp, caption: cap, milliseconds: milliseconds});
-      }
-      let existingDiv = document.getElementsByClassName('videorow')[0];
-      const flexContainer = document.createElement('div');
-      flexContainer.classList.add('flex-container');
-  
-      flexContainer.appendChild(existingDiv);
-  
-      const injectElement = document.createElement('div');
-      injectElement.className = "caption-box";
-      for (obj of caption_divs)
-      {
-        const preElement = document.createElement('pre');
-        //preElement.className = "hover";
-        preElement.innerHTML = obj.timestamp + "\n"+obj.caption;
-        preElement.dataset.time = obj.milliseconds;
-        preElement.addEventListener('click', function(e)
-        {
-          let video = document.getElementsByTagName("video")[0];;
-          video.currentTime = e.srcElement.dataset.time;
-        })
-        injectElement.appendChild(preElement);
-
-      }
-      flexContainer.appendChild(injectElement);
-      document.body.appendChild(flexContainer);
-    }
-  //  else{
-  //     let iframe = document.getElementById("/html/body/ic-Layout-contentMain[3]/div[2]/div[2]/div[3]/div[1]/div/div[1]/iframe", document,
-  //     null,
-  //     XPathResult.FIRST_ORDERED_NODE_TYPE,
-  //     null).singleNodeValue;
-  //     let doc = ifrm.contentWindow.document;
-  //     console.log(doc);
-  //   // const flexContainer = document.createElement('div');
-  //   // flexContainer.classList.add('flex-container');
-  //   // flexContainer.appendChild(iframe);
-  //   // const injectElement = document.createElement('div');
-  //   // const preElement = document.createElement('pre')
-  //   // preElement.innerHTML = msg.captions;
-  //   // injectElement.className = "caption-box";
-  //   // injectElement.appendChild(preElement);
-  //   // flexContainer.appendChild(injectElement);
-  //   // document.body.appendChild(flexContainer);
-    
-  //  }
-   
-  }
-});
-
-let vid = document.getElementsByTagName("video")[0];
-vid.ontimeupdate = function() {
+function auto_scroll() {
   let existingDiv = document.getElementsByClassName('caption-box')[0];
   let children = existingDiv.childNodes;
   // console.log(children)
   let mindif = 10000;
   let cur = {};
   seekedTime = vid.currentTime;
-  for (cap of children)
-  {
+  for (cap of children) {
     let dif = seekedTime - cap.dataset.time;
-    if (dif >=0 && dif <= mindif)
-    {
+    if (dif >= 0 && dif <= mindif) {
       mindif = dif;
       cur = cap;
     }
     cap.style.backgroundColor = "white"
   }
-  cur.scrollIntoView({ block: 'center',  behavior: 'smooth' });
-  cur.style.backgroundColor = "yellow";  
-  
-
+  cur.scrollIntoView({ block: 'center', behavior: 'smooth' });
+  cur.style.backgroundColor = "yellow";
 };
+let vid = document.getElementsByTagName("video")[0];
+vid.ontimeupdate = auto_scroll;
+chrome.runtime.onMessage.addListener(async function (msg, sender, sendResponse) {
 
-vid.onseeked = function() {
-  let existingDiv = document.getElementsByClassName('caption-box')[0];
-  let children = existingDiv.childNodes;
-  console.log(children)
-  let mindif = 10000;
-  let cur = {};
-  seekedTime = vid.currentTime;
-  for (cap of children)
-  {
-    let dif = seekedTime - cap.dataset.time;
-    if (dif >=0 && dif <= mindif)
-    {
-      mindif = dif;
-      cur = cap;
+  //console.log(msg)
+  if (msg.captions) {
+    //modified_captions = addNewlineBeforeTimestamps(msg.captions)
+    //console.log(msg.captions)
+    if (msg.source.includes("utexas")) {
+      let caption_divs = [];
+      let captions = msg.captions;
+      let caption_chunks = captions.split("\n\n");
+      caption_chunks.shift();
+      for (chunk of caption_chunks) {
+        let s = chunk.split("\n");
+        console.log(s)
+        let timestamp = s[0].split(" ")[0];
+        let cap = s.slice(1).join(" ");
+        let milliseconds = timestampToMilliseconds(timestamp);
+        caption_divs.push({ timestamp: timestamp, caption: cap, milliseconds: milliseconds });
+      }
+      let existingDiv = document.getElementsByClassName('videorow')[0];
+      const flexContainer = document.createElement('div');
+      flexContainer.classList.add('flex-container');
+
+      flexContainer.appendChild(existingDiv);
+
+      const injectElementOutside = document.createElement('div');
+      injectElementOutside.classList.add("captionBlob");
+      const injectElement = document.createElement('div');
+      injectElement.className = "caption-box";
+      for (obj of caption_divs) {
+        const preElement = document.createElement('pre');
+        //preElement.className = "hover";
+        preElement.innerHTML = obj.timestamp + "\n" + obj.caption;
+        preElement.dataset.time = obj.milliseconds;
+        preElement.addEventListener('click', function (e) {
+          let video = document.getElementsByTagName("video")[0];;
+          video.currentTime = e.srcElement.dataset.time;
+        })
+        injectElement.appendChild(preElement);
+
+      }
+      injectElementOutside.appendChild(injectElement);
+      const buttonDiv = document.createElement('div');
+      buttonDiv.classList.add("right");
+      const button = document.createElement('button');
+      button.innerHTML = "Auto-Scroll"
+      button.onclick = function(){
+        if(!scroll)
+        {
+          if (vid)
+          vid.ontimeupdate = auto_scroll;
+          scroll = true;
+          button.style.background = "white";
+        }
+        else{
+          if (vid)
+          vid.ontimeupdate = function () {
+            let existingDiv = document.getElementsByClassName('caption-box')[0];
+            let children = existingDiv.childNodes;
+            // console.log(children)
+            let mindif = 10000;
+            let cur = {};
+            seekedTime = vid.currentTime;
+            for (cap of children) {
+              let dif = seekedTime - cap.dataset.time;
+              if (dif >= 0 && dif <= mindif) {
+                mindif = dif;
+                cur = cap;
+              }
+              cap.style.backgroundColor = "white"
+            }
+            //cur.scrollIntoView({ block: 'center', behavior: 'smooth' });
+            cur.style.backgroundColor = "yellow";
+          };
+          scroll = false;
+          button.style.backgroundColor = "blue";
+        }
+
+      }
+      button.classList.add("button-4");
+      buttonDiv.appendChild(button)
+      injectElementOutside.appendChild(buttonDiv)
+      flexContainer.appendChild(injectElementOutside);
+
+      document.body.appendChild(flexContainer);
+      //document.body.appendChild(button);
     }
   }
-  cur.scrollIntoView({ block: 'center' });
+});
 
-};
+if (vid)
+  vid.onseeked = function () {
+    let existingDiv = document.getElementsByClassName('caption-box')[0];
+    let children = existingDiv.childNodes;
+    console.log(children)
+    let mindif = 10000;
+    let cur = {};
+    seekedTime = vid.currentTime;
+    for (cap of children) {
+      let dif = seekedTime - cap.dataset.time;
+      if (dif >= 0 && dif <= mindif) {
+        mindif = dif;
+        cur = cap;
+      }
+    }
+    cur.scrollIntoView({ block: 'center' });
+
+  };
